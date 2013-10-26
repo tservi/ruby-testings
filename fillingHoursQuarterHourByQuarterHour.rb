c2c1fa6c4faa6b67cc7b804b67a265b6539ce669
Jean Tinguely Awais aka t-servi.com <jeantinguelyawais@gmail.com>
2013-10-26 23:51:25 +0200

    changing the spaces

--- fillingHoursQuarterHourByQuarterHour.rb
+++ fillingHoursQuarterHourByQuarterHour.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-(8..21).to_a.each {|hour|
-  ['00', '15' , '30' , '45' ].each {|quarter|
+(8..21).to_a.each { |hour|
+  ['00', '15' , '30' , '45' ].each { |quarter|
     print "#{format("%02d",hour)}:#{quarter} \n"  
   } 
 }
